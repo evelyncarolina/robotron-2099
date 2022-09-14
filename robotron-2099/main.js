3d1d8d5b37194c7dd7de37113ab9ec49b537bb93
evelyncarolina <evelyncarolina.contato@gmail.com>
2022-09-14 19:37:00 -0300

    [Feature - Task: Melhorando o código]

--- robotron-2099/main.js
+++ robotron-2099/main.js
@@ -5,12 +5,14 @@ const controle = document.querySelectorAll(".controle-ajuste")
 
 controle.forEach((elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent)
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
     })
 })
 
-function manipulaDados(operacao){
-    if(operacao === "subtrair"){
+function manipulaDados(operacao, controle){
+    const peca = controle.querySelector(".controle-contador")
+
+    if(operacao === "-"){
         braco.value = parseInt(braco.value) - 1
     } else{
         braco.value = parseInt(braco.value) + 1
