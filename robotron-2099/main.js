8584180447a990504543e120340fde7ed006f0fc
evelyncarolina <evelyncarolina.contato@gmail.com>
2022-09-14 19:28:49 -0300

    [Feature - Task: Melhorando o codigos de sub e somar]

--- robotron-2099/main.js
+++ robotron-2099/main.js
@@ -3,8 +3,11 @@ const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
 const controle = document.querySelectorAll(".controle-ajuste")
 
-somar.addEventListener("click", () =>{manipulaDados("somar")})
-subtrair.addEventListener("click", () =>{manipulaDados("subtrair")})
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
+})
 
 function manipulaDados(operacao){
     if(operacao === "subtrair"){
