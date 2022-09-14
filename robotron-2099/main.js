8f98d2b8c4401204c3b92e4915080f1d8ee68107
evelyncarolina <evelyncarolina.contato@gmail.com>
2022-09-14 18:31:29 -0300

    [Feature - Task: Add somar e subtrair ao braço do robotron]

---
+++ robotron-2099/main.js
@@ -0,0 +1,27 @@
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
+const controle = document.querySelectorAll(".controle-ajuste")
+
+somar.addEventListener("click", () =>{manipulaDados("somar")})
+subtrair.addEventListener("click", () =>{manipulaDados("subtrair")})
+
+function manipulaDados(operacao){
+    if(operacao === "subtrair"){
+        braco.value = parseInt(braco.value) - 1
+    } else{
+        braco.value = parseInt(braco.value) + 1
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
